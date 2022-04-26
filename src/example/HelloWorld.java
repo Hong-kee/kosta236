230f072e5f5f413018b497c3d71629b4624c933d
JK <alswkdrb1@naver.com>
2022-04-26 12:59:57 +0900

    12:59

--- src/example/HelloWorld.java
+++ src/example/HelloWorld.java
@@ -1,7 +1,7 @@
 package example;public class HelloWorld {
   public String sayHelloWorldFrom(String from) {
     String result = "Hello, world, from " + from;
-    result += "i'm really hungry...";
+    result += "i'm really really hungry...!!";
     System.out.println(result);
     return result;
   }
